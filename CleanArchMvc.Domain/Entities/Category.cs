7b0336ce7c28c3793f76bab765e8d5e47f661cb6
André Cabral <andrecabrall@gmail.com>
2022-12-03 14:26:12 -0300

    FirstCommit

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+    
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public ICollection<Product> products { get; private set; }
+
+        private void ValidateDomain(string name) {
+
+            DomainExceptionValidation
+        }
+
+    }
+}
+
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CleanArchMvc.Domain.Entities
+{
+	public sealed class Product
+	{
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public int categoryId { get; private set; }
+        public Category category { get; private set; }
+
+    }
+}
+
